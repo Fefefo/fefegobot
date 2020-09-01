eecf461d80a921c79703e3680096fde13cf5ce74
Fefefo <tardiolifederico@gmail.com>
2020-09-01 03:04:06 +0200

    removed []

--- main.go
+++ main.go
@@ -422,7 +422,7 @@ func main() {
 					if strings.HasSuffix(query, ".") {
 						urb := getUrb(strings.ReplaceAll(query, ".", ""))
 						for i := 0; i < len(urb.List); i++ {
-							a := tgbotapi.NewInlineQueryResultArticleHTML("urban"+strconv.Itoa(i), urb.List[i].Word, "Title: <a href='"+urb.List[i].Permalink+"'>"+urb.List[i].Word+"</a>\n\n"+urb.List[i].Def+"\n\nAuthor: "+urb.List[i].Author)
+							a := tgbotapi.NewInlineQueryResultArticleHTML("urban"+strconv.Itoa(i), urb.List[i].Word, "Title: <a href='"+urb.List[i].Permalink+"'>"+urb.List[i].Word+"</a>\n\n"+strings.ReplaceAll(strings.ReplaceAll(urb.List[i].Def, "[", ""), "]", "")+"\n\nAuthor: "+urb.List[i].Author)
 							a.Description = urb.List[i].Author
 							array = append(array, a)
 						}
